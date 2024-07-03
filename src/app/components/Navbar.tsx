6463273d117869d617af2107106cd4eef4938a80
juanfsouza <juanfsouza@gmail.com>
2024-07-03 19:59:16 -0300

    Auth fix

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,12 +1,15 @@
+"use client"
+import Image from 'next/image';
+import Link from 'next/link';
+import MobileMenu from './MobileMenu';
+import { ClerkLoading, SignedOut } from '@clerk/nextjs';
+import { ClerkLoaded, SignedIn, UserButton } from '@clerk/clerk-react';
 
-import Link from 'next/link'
-import MobileMenu from './MobileMenu'
-
-export const Navbar = () => {
+const Navbar = () => {
     return (
     <div className='h-24 flex items-center justify-between'>
         {/* LEFT BAR */}
-        <div className=''>
+        <div className='md:hidden lg:block w-[20%]'>
             <Link 
                 href="/" 
                 className='font-bold text-xl text-blue-600'>
@@ -14,11 +17,52 @@ export const Navbar = () => {
             </Link>
         </div>
         {/* CENTER BAR */}
-        <div className='hidden'></div>
+        <div className='hidden md:flex w-[50%] text-sm'>
+            {/* LINKS */}
+            <div className='flex gap-6 text-gray-600'>
+                <Link href="/" className='flex gap-2'>
+                    <Image src="/home.png" alt="Homepage" width={16} height={16} className='w-4 h-4' />
+                    <span>Homepage</span>
+                </Link>
+                <Link href="/" className='flex gap-2'>
+                    <Image src="/friends.png" alt="Friends" width={16} height={16} className='w-4 h-4' />
+                    <span>Friends</span>
+                </Link>
+                <Link href="/" className='flex gap-2'>
+                    <Image src="/stories.png" alt="Stories" width={16} height={16} className='w-4 h-4' />
+                    <span>Stories</span>
+                </Link>
+            </div>
+        </div>
         {/* RIGHT BAR */}
-        <div className=''>
+        <div className='w-[30%] flex items-center gap-4 xl:gap-8 justify-end'>
+            <ClerkLoading>
+                <div className='inline-block h-4 w-4 animate-spin rounded-full border-2 border-gray-500 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white' />
+            </ClerkLoading>
+            <ClerkLoaded>
+                <SignedIn>
+                    <div className='cursor-pointer'>
+                        <Image src="/people.png" alt="" width={20} height={20} />
+                    </div>
+                    <div className='cursor-pointer'>
+                        <Image src="/messages.png" alt="" width={20} height={20} />
+                    </div>
+                    <div className='cursor-pointer'>
+                        <Image src="/notifications.png" alt="" width={20} height={20} />
+                    </div>
+                    <UserButton />
+                </SignedIn>
+                <SignedOut>
+                    <div className="flex items-center gap-2 text-sm">
+                        <Image className="/work.png" alt="" width={20} height={20} src={''} />
+                        <Link href="/sign-in">Login/Register</Link>
+                    </div>
+                </SignedOut>
+            </ClerkLoaded>
             <MobileMenu />
         </div>
     </div>
   )
 }
+
+export default Navbar;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Navbar } from "./components/Navbar";
+import Navbar from "./components/Navbar";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,13 +17,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <div className="w-full bg-white md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-          <Navbar />
-        </div>
-          {children}
-        </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <div className="w-full bg-white md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+            <Navbar />
+          </div>
+            {children}
+          </body>
+      </html>
+    </ClerkProvider>
   );
 }
